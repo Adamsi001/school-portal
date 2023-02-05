963a7fae092b8ad016363acf4b5b9f39510e7bb3
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-02-05 07:59:27 +0100

    feat Add id to users serializers

--- users/serializers.py
+++ users/serializers.py
@@ -6,30 +6,30 @@ from .models import BaseUser as User, Admin, Lecturer, Staff, Student, StudentAd
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'is_staff', "user_type", "staff_type", 'faculty', 'department', 'level', 'matric_number', )
+        fields = ('id', 'email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'is_staff', "user_type", "staff_type", 'faculty', 'department', 'level', 'matric_number', )
 
 
 class AdminSerializer(ModelSerializer):
     class Meta:
         model = Admin
-        fields = ('email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'is_superuser')
+        fields = ('id', 'email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'is_superuser')
 
 class StudentSerializer(ModelSerializer):
     class Meta:
         model = Student
-        fields = ('email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'faculty', 'department', 'level', 'matric_number',)
+        fields = ('id', 'email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'faculty', 'department', 'level', 'matric_number',)
 
 class StaffSerializer(ModelSerializer):
     class Meta:
         model = Staff
-        fields = ('email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'staff_type',)
+        fields = ('id', 'email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'staff_type',)
 
 class LecturerSerializer(ModelSerializer):
     class Meta:
         model = Lecturer
-        fields = ('email',  'user_id', 'first_name', 'middle_name', 'last_name',  'gender', 'is_active', 'faculty', 'department',)
+        fields = ('id', 'email',  'user_id', 'first_name', 'middle_name', 'last_name',  'gender', 'is_active', 'faculty', 'department',)
 
 class StudentAdviserSerializer(ModelSerializer):
     class Meta:
         model = StudentAdviser
-        fields = ('email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'faculty', 'department', 'level',)
+        fields = ('id', 'email',  'user_id', 'first_name', 'middle_name', 'last_name', 'gender', 'is_active', 'faculty', 'department', 'level',)
