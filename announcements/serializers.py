af5645824a8108243542d32633f6887d28ba6565
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-02-07 21:48:43 +0100

    feat add change how announcements work
    now the creator is linked automatically on creation
    if it is from a student adviser, it is sent to the students automatically
    list action only shows the announcements created by the user or sent to them

--- announcements/serializers.py
+++ announcements/serializers.py
@@ -1,9 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+
+from users.serializers import UserSerializer
 
 from .models import Announcement
 
 
 class AnnouncementSerializer(ModelSerializer):
+    created_by = SerializerMethodField()
+
     class Meta:
         model = Announcement
         fields = "__all__"
+
+    def get_created_by(self, announcement):
+        return UserSerializer(announcement.created_by).data
--- announcements/views.py
+++ announcements/views.py
@@ -1,10 +1,28 @@
 from rest_framework import viewsets
 
+from django.contrib.auth import get_user_model
+
 from .models import Announcement
 from .serializers import AnnouncementSerializer
+
+User = get_user_model()
 
 
 class AnnouncementViewSet(viewsets.ModelViewSet):
     queryset = Announcement.objects.all()
     serializer_class = AnnouncementSerializer
     permission_classes = []
+
+    def get_queryset(self):
+        if self.action == "list":
+            return self.queryset.filter(created_by=self.request.user) | self.queryset.filter(sent_to=self.request.user)
+        return super().get_queryset()
+
+    def perform_create(self, serializer):
+        user = self.request.user  # current user
+        announcement = serializer.save(created_by=user)
+
+        if user.is_student_adviser:
+            announcement.sent_to.add(*User.objects.filter(user_type=User.UserTypes.STUDENT,
+                                     department=user.department, faculty=user.faculty, level=user.level))
+        announcement.save()
